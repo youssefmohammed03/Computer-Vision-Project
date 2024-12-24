6acfd32736ab1ee2482a749b7f2d4187cffa4c07
Youssef <21P0094@eng.asu.edu.eg>
2024-12-24 22:49:12 +0200

    .

--- decoder.py
+++ decoder.py
@@ -1,72 +1,74 @@
 import cv2
 import numpy as np
 
-# 0 means narrow, 1 means wide
-NARROW = "0"
-WIDE = "1"
-code11_widths = {
-    "00110": "Stop/Start",
-    "10001": "1",
-    "01001": "2",
-    "11000": "3",
-    "00101": "4",
-    "10100": "5",
-    "01100": "6",
-    "00011": "7",
-    "10010": "8",
-    "10000": "9",
-    "00001": "0",
-    "00100": "-",
-}
+def decode(image_path):
+    # 0 means narrow, 1 means wide
+    NARROW = "0"
+    WIDE = "1"
+    code11_widths = {
+        "00110": "Stop/Start",
+        "10001": "1",
+        "01001": "2",
+        "11000": "3",
+        "00101": "4",
+        "10100": "5",
+        "01100": "6",
+        "00011": "7",
+        "10010": "8",
+        "10000": "9",
+        "00001": "0",
+        "00100": "-",
+    }
 
-your_cropped_image = cv2.imread("Cropped_Barcode.jpg", cv2.IMREAD_GRAYSCALE)
 
-# Get the average of each column in your image
-mean = your_cropped_image.mean(axis=0)
+    your_cropped_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-# Set it to black or white based on its value
-mean[mean <= 127] = 1
-mean[mean > 128] = 0
+    # Get the average of each column in your image
+    mean = your_cropped_image.mean(axis=0)
 
-# Convert to string of pixels in order to loop over it
-pixels = ''.join(mean.astype(np.uint8).astype(str))
+    # Set it to black or white based on its value
+    mean[mean <= 127] = 1
+    mean[mean > 128] = 0
 
-# Need to figure out how many pixels represent a narrow bar
-narrow_bar_size = 0
-for pixel in pixels:
-    if pixel == "1":
-        narrow_bar_size += 1
-    else:
-        break
+    # Convert to string of pixels in order to loop over it
+    pixels = ''.join(mean.astype(np.uint8).astype(str))
 
-wide_bar_size = narrow_bar_size * 2
+    # Need to figure out how many pixels represent a narrow bar
+    narrow_bar_size = 0
+    for pixel in pixels:
+        if pixel == "1":
+            narrow_bar_size += 1
+        else:
+            break
 
-digits = []
-pixel_index = 0
-current_digit_widths = ""
-skip_next = False
+    wide_bar_size = narrow_bar_size * 2
 
-while pixel_index < len(pixels):
+    digits = []
+    pixel_index = 0
+    current_digit_widths = ""
+    skip_next = False
 
-    if skip_next:
-        pixel_index += narrow_bar_size
-        skip_next = False
-        continue
+    while pixel_index < len(pixels):
 
-    count = 1
-    try:
-        while pixels[pixel_index] == pixels[pixel_index + 1]:
-            count += 1
-            pixel_index += 1
-    except:
-        pass
-    pixel_index += 1
+        if skip_next:
+            pixel_index += narrow_bar_size
+            skip_next = False
+            continue
 
-    current_digit_widths += NARROW if count == narrow_bar_size else WIDE
+        count = 1
+        try:
+            while pixels[pixel_index] == pixels[pixel_index + 1]:
+                count += 1
+                pixel_index += 1
+        except:
+            pass
+        pixel_index += 1
 
-    if current_digit_widths in code11_widths:
-        digits.append(code11_widths[current_digit_widths])
-        current_digit_widths = ""
-        skip_next = True  # Next iteration will be a separator, so skip it
+        current_digit_widths += NARROW if count == narrow_bar_size else WIDE
 
-print(digits)
+        if current_digit_widths in code11_widths:
+            digits.append(code11_widths[current_digit_widths])
+            current_digit_widths = ""
+            skip_next = True  # Next iteration will be a separator, so skip it
+
+    print(digits)
